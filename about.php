beefff2a0fc1683a61594e7b1b9684f45afce11e
Dylan Cressy <dylancressy5@gmail.com>
2016-05-04 16:06:56 -0400

    Finished

--- about.php
+++ about.php
@@ -38,13 +38,13 @@
 		</div>			
 		<div id="nav">
 			<ul>
-				<li><a href="flixpix.html">Home</a></li>
-				<li><a href="news.html">News</a></li>
+				<li><a href="flixpix.php">Home</a></li>
+				<li><a href="news.php">News</a></li>
 				<li><a href="">Bookmarks</a></li>
 				<li><a href="">Log In</a></li>
-				<li><a href="">Forum</a></li>
-				<li><a href="about.html">About</a></li>
-				<li><a href="contact.html">Contact</a></li>
+				<li><a href="forum.php">Forum</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="contact.php">Contact</a></li>
 			</ul>
 		</div>
 			<div id="feature">
