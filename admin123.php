0a6bd6591cdc372cc8ed7d8620c37620df8f47d6
CharLi1738 <charlesli@Charless-MacBook-Pro.local>
2016-05-04 15:54:36 -0400

    updated admin (style)

--- admin123.php
+++ admin123.php
@@ -34,10 +34,13 @@ $sql = "SELECT user_id, user_name, user_email, user_pass FROM users";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-     echo "<table><tr><th>ID</th><th>Name</th><th>Email</th><th>Password</th></tr>";
+     echo "<table>
+     <tr><th>ID</th><th>Name</th><th>Email</th><th>Password</th><th>Action</th></tr>\n";
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<tr><td>" . $row["user_id"]. "</td><td>" . $row["user_name"]. "</td><td>" . $row["user_email"] . "</td><td>" . $row["user_pass"] . "</td></tr>";
+         echo " <tr><td>" . $row["user_id"]. "</td><td>" . $row["user_name"]. "</td><td>" 
+         . $row["user_email"] . "</td><td>" . $row["user_pass"] . "</td><td>" 
+         . "<a href='delete.php?user_id=" . $row['user_id'] . "'>Delete</a>" . "</td></tr>\n";
      }
      echo "</table>";
 } else {
@@ -45,14 +48,14 @@ if ($result->num_rows > 0) {
 }
 
 
-$sql2 = "SELECT name, comment FROM comments";
+$sql2 = "SELECT c_id, name, comment FROM comments";
 $result = $conn->query($sql2);
 
 if ($result->num_rows > 0) {
-     echo "<table><tr><th>Name</th><th>Comment</th></tr>";
+     echo "<table><tr><th>Name</th><th>Comment</th><th>Action</th></tr>";
      // output data of each row
      while($row = $result->fetch_assoc()) {
-         echo "<tr><td>" . $row["name"]. "</td><td>" . $row["comment"] . "</td></tr>";
+         echo "<tr><td>" . $row["name"]. "</td><td>" . $row["comment"] . "</td><td>" . "<a href='delete.php?c_id=" . $row['c_id'] . "'>Delete</a>" . "</td></tr>";
      }
      echo "</table>";
 } else {
