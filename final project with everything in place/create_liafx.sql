1983ae7bf94d5110a6964dd33503699df2876d7b
CharLi1738 <charlesli@Charless-MacBook-Pro.local>
2016-05-11 19:46:52 -0400

    dragging stuff in here for convenience
    done

---
+++ final project with everything in place/create_liafx.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 26, 2016 at 09:43 AM
+-- Server version: 5.1.73
+-- PHP Version: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `liafx`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(5) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(25) NOT NULL,
+  `user_email` varchar(35) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+
+
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `c_id` int(5),
+  `name` varchar(100),
+  `title` varchar(100),
+  `comment` varchar(255)
+  
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 
+
+
+
+
+
+
+
---
+++ final project with everything in place/populate_liafx.sql
@@ -0,0 +1,17 @@
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_pass`) VALUES
+(1, 'test', 'test@testmail.com', '202cb962ac59075b964b07152d234b70'),
+(2, 'charlesli', '123@bc.edu', '040b7cf4a55014e185813e0644502ea9'),
+(3, 'alabama amba', 'alabma@bc.edu', 'c96dd568316deb9d8c7dec73b4c27cbb'),
+(4, 'pikachu', 'pika@bc.edu', 'b8f78c9547701c3ab0d6d809cb05252a');
+
+
+INSERT INTO `rating`(`c_id`, `name`, `title` `comment`) VALUES 
+(1,'bob','very helpful!','yes'),
+(2,'mob','meh','yes'),
+(3,'kob','more movies would be better','yes'),
+(4,'lob','very helpful!','yes')
+
+
+
+
+
